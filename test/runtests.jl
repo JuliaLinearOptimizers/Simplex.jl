923662d60699ecf55a9ff528b68cc65257fa98a7
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-10-22 18:14:29 -0300

    Add Project.toml and fix for 1.2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,4 @@
-using Pkg
-
-Pkg.add("JuMP")
-Pkg.add("GLPK")
-
 using Simplex, Test, JuMP, GLPK, Random, LinearAlgebra, SparseArrays
 
-  include("test_simplexluup.jl")
-  include("test_simplexinv.jl")
+include("test_simplexluup.jl")
+include("test_simplexinv.jl")
